c8f05e07940268161122776a32ffc3a8fa4d0377
topias liljegren <topias.liljegren@gmail.com>
2024-09-15 16:50:14 +0300

    2.6 tehty

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+	const [newName, setNewName] = useState('')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const addPerson = (event) => {
+		event.preventDefault()
+		const personObject = {
+			name: newName,
+		}
+		setPersons(persons.concat(personObject))
+		setNewName('')
+	}
+
+	const handleNameChange = (event) => {
+		console.log(event.target.value)
+		setNewName(event.target.value)
+	}
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addPerson} onChange={handleNameChange}>
+				<div>
+					name: <input value={newName} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{persons.map(name =>
+				<div>{name.name}</div>
+			)}
+		</div>
+	)
+
 }
 
 export default App
