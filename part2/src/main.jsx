388e1b2d99087431577d6078da826fb373ec7892
Topias Liljegren <topias.liljegren@gmail.com>
2024-12-09 11:54:30 +0200

    2.11 tehty

--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
 
 import App from './App'
 
