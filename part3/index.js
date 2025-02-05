1fc4513821dd9e8532d4d97420960e5a59e23469
Topias Liljegren <topias.liljegren@gmail.com>
2025-02-05 14:24:34 +0200

    3.15

--- part3/index.js
+++ part3/index.js
@@ -34,12 +34,13 @@ app.get('/api/persons/:id', (request, response) => {
 	})
 })
 
-// app.delete('/api/persons/:id', (request, response) => {
-// 	const id = request.params.id
-// 	persons = persons.filter(person => person.id !== id)
-//
-// 	response.status(204).end()
-// })
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then(result => {
+			response.status(201).end()
+		})
+		.catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
 	const reqPer = request.body
