ad927ce9ca1c5fc9a096e585d28ef6af852e87e3
Topias Liljegren <topias.liljegren@gmail.com>
2025-02-11 12:36:59 +0200

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,7 +10,20 @@ const totalLikes = (blogs) => {
 	return total
 }
 
+const favoriteBlog = (blogs) => {
+	var fav = { title: "", author: "", likes: 0 }
+	blogs.forEach(blog => {
+		// console.log(blog)
+		// console.log(fav)
+		if (blog.likes > fav.likes) {
+			fav = blog
+		}
+	})
+	return { title: fav.title, author: fav.author, likes: fav.likes }
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 }
